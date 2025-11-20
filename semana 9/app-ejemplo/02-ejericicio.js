cf62dc57ff9ef6514739f490b889bcc10f551604
Silvana <labateriadedowoon@gmail.com>
2025-11-20 15:58:14 -0500

    efjf

---
+++ semana 9/app-ejemplo/02-ejericicio.js
@@ -0,0 +1,30 @@
+//suma 
+let monitor = 100;
+let cpu = 120;
+let memoria = 350;
+
+let precio_total= monitor + cpu + memoria;
+console.log("el precio total es: ", precio_total);
+
+let adicionales =50 + 10;
+console.log("el precio de los adicionales es: ", adicionales);
+
+//resta
+
+let descuento = 25;
+let preciocondescuento = precio_total - descuento;
+console.log("el precio con descuento es: ", preciocondescuento);
+
+//dividir en cuotas
+let cuotas = preciocondescuento/2;
+console.log("el precio en 2 cuotas es: ", cuotas);
+
+//multiplicar
+let preciofinal = precio_total * 3
+console.log("precio por 3 unidades: ", preciofinal);
+
+//++variable
+
+let numero = 10;
+console.log(numero++);
+console.log(++numero);
--- semana 9/app-ejemplo/app.js
+++ semana 9/app-ejemplo/app.js
@@ -5,4 +5,11 @@ console.log("mi nombre es " , nombre);
 
 let edad = 25;
 console.log("mi edad es " , edad);
+let numero =20;
+let nota = "20";
 
+console.log(nota);
+console.log(typeof(nota));
+
+console.log(numero);
+console.log(typeof(numero));
